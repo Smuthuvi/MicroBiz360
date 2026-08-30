85585da20078fc1e2bbebd9819d7cb23984fbc8e
Smuthuvi <smuthuvi@yahoo.com>
2026-08-30 17:42:06 +0300

    Navigation 001: Establish navigation architecture foundation

--- app/src/main/java/com/muthuvi/microbiz360/MainActivity.kt
+++ app/src/main/java/com/muthuvi/microbiz360/MainActivity.kt
@@ -4,44 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.muthuvi.microbiz360.navigation.MicroBiz360NavHost
 import com.muthuvi.microbiz360.ui.theme.MicroBiz360Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
             MicroBiz360Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MicroBiz360NavHost()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MicroBiz360Theme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/muthuvi/microbiz360/navigation/MicroBiz360NavHost.kt
@@ -0,0 +1,22 @@
+package com.muthuvi.microbiz360.navigation
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun MicroBiz360NavHost() {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = "dashboard"
+    ) {
+        composable("dashboard") {
+            Text(text = "MicroBiz360 Dashboard")
+        }
+    }
+}
+
